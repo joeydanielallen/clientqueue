c5372521b6cf73631d0630394a453baae0cae004
Joey <jda144@hotmail.com>
2018-11-11 12:50:32 -0600

    adding dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'workQ';
+  title = 'Client Queue Example';
+  queue_complete = 'import complete';
+  queue_working = 'importing';
+  is_importing = false;
+
+  items = [
+    {name:'Record one', status:''},
+    {name:'Record two', status:''},
+    {name:'Record three', status:''},
+    {name:'Record four', status:''},
+    {name:'Record five', status:''},
+    {name:'Record six', status:''},
+    {name:'Record seven', status:''}
+  ];
+
+  doIt(): void{
+    this.is_importing = true;
+    this.doWork(0);
+  }
+
+  doWork(index: number): void {
+    if (index > 0) {
+
+      if(index === 3) {
+        this.items[index-1].status = 'Error: peeps be cray cray';
+      }
+      else if(index === 7) {
+      this.items[index-1].status = 'Error: kids be cray cray';
+      }
+      else {
+        this.items[index-1].status = this.queue_complete;
+      }
+    }
+
+    if (index === this.items.length){
+      this.is_importing = false;
+       return;
+    }
+
+    this.items[index].status = this.queue_working;
+    window.setTimeout(() => this.doWork(index+1), 2000);
+  }
 }
